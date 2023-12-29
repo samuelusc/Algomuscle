ce701ff537dea346dc5f64eff0d52174d2cea744
samuelusc <97491762+samuelusc@users.noreply.github.com>
2023-12-29 03:29:33 -0800

    Time: 253 ms (5.35%), Space: 18.2 MB (8.1%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.py
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -1,66 +1,67 @@
-# Build myself but not necessary in leetcode
-# class ListNode:
-#     def __init__(self, val=0,next= None):
-#         self.val = val
-#         self.next = next
-
 class MyLinkedList:
-
+    #design scratch 
     def __init__(self):
+        # Create a dummy node
         self.dummy_node = ListNode()
-        self.size  = 0    
+        # Initialize node counter and length
+        self.size = 0
 
     def get(self, index: int) -> int:
+        # Boundary check
         if index < 0 or index >= self.size:
-            return -1
-        
-        cur_node = self.dummy_node.next
-        
+            return  -1
+
+        # Start from the head, there will be at least one node
+        current_node = self.dummy_node.next
+
+        # Iterate to the next node based on index
         for _ in range(index):
-            cur_node = cur_node.next
-        return cur_node.val
+            current_node = current_node.next
+        
+        return current_node.val
 
     def addAtHead(self, val: int) -> None:
-        self.addAtIndex(0,val)
- 
+        # Invoke addAtIndex and assign (0, val)
+        self.addAtIndex(0, val)
+
     def addAtTail(self, val: int) -> None:
+        # Invoke addAtIndex and assign (self.size, val)
         self.addAtIndex(self.size, val)
 
-
     def addAtIndex(self, index: int, val: int) -> None:
+        # Check for index out of bounds
         if index < 0 or index > self.size:
-             return
+            return
         
+        # Assign the dummy node to predecessor
         predecessor = self.dummy_node
+
+        # Find the predecessor of the node to be added
         for _ in range(index):
             predecessor = predecessor.next
+        
+        # Create the new node and maintain the predecessor's link
+        add_node = ListNode(val, predecessor.next)
+        # Link the predecessor with the new node
+        predecessor.next = add_node
 
-        new_node = ListNode(val, predecessor.next)
-        predecessor.next = new_node
-
+        # Don't forget to increment self.size by 1 for each added node
         self.size += 1
 
-
-
     def deleteAtIndex(self, index: int) -> None:
-        
-        if index < 0 or index >= self.size: 
+        # Check boundaries
+        if index < 0 or index >= self.size:
             return 
         
+        # Like in addAtIndex, first find the predecessor
         predecessor = self.dummy_node
         for _ in range(index):
             predecessor = predecessor.next
         
-        predecessor.next = predecessor.next.next       
+        # The predecessor skips the node to be deleted
+        predecessor.next = predecessor.next.next
+        # Don't forget to decrement self.size by 1
         self.size -= 1
-
-        # ensuring the integrity of linked list and preventing 
-        # potential issues or we can do below
-
-        # delete_node = predecessor.next
-        # predecessor.next = delete_node.next
-        # delete_node.next = None
-
 
 
 # Your MyLinkedList object will be instantiated and called as such:
