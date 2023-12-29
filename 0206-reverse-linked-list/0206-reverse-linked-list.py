1cac26422a040b844343f711a3f1dbf59060f179
samuelusc <97491762+samuelusc@users.noreply.github.com>
2023-12-29 01:14:55 -0800

    Time: 72 ms (5.09%), Space: 18.4 MB (27.32%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+            
+        # Initialize the dummy node and the current node.
+        dummy_node = ListNode()
+        cur_node = head
+
+        #不断读取现在的节点    
+        while cur_node:
+            # Save the link to the rest of the list in temp_next
+            temp_next = cur_node.next
+
+            # Isolate cur_node by pointing it to the beginning of the reversed list.            
+            cur_node.next = dummy_node.next
+            
+            # Link the dummy node to the newly isolated node,
+            dummy_node.next = cur_node
+            
+            # Move to the next node in the original list using the saved link.
+            cur_node = temp_next
+        
+        return dummy_node.next
