6392006b0bcac149f581c4ff003f0441ac808461
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-01 00:24:59 -0800

    Time: 65 ms (5.72%), Space: 17.2 MB (16.33%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,15 +1,19 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set()
+        def next_number(num):
+            total_sum = 0
+            # or we can use sum(int(digit) ** 2 for digit in str(num))
+            while num:
+                num, remainder = divmod(num, 10)
+                total_sum += remainder ** 2
+            return total_sum
 
-        while n!= 1:
-            n = sum(int(i) ** 2 for i in str(n))
+        slow = n
+        fast = next_number(n)
+        # tend to make mistake to set next_number(n)
+        while slow != fast:
+            slow = next_number(slow)
+            fast = next_number(next_number(fast))
+
+        return slow == 1
             
-            if n in seen:
-                return False
-            else:
-                seen.add(n)
-
-        return True
-
-
