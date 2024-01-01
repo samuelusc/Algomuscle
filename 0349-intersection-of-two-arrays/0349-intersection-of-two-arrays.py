028cbfd1e83e659bb82b5baeaadebfd19b0494f3
samuelusc <97491762+samuelusc@users.noreply.github.com>
2023-12-31 22:23:49 -0800

    Time: 64 ms (17.43%), Space: 17.5 MB (6.72%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashTable = dict()
+        res = set()
+        for num in nums1:
+            hashTable[num] = hashTable.get(num, 0) + 1
+
+        for num in nums2:
+            if num in hashTable:
+                res.add(num)
+
+        return list(res)
+                
