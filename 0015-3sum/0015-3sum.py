83fab4654eebff7cc3cf401a680f869b75272a59
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-01 22:33:44 -0800

    Update 0015-3sum.py

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,11 +2,15 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res =[]
         nums.sort()
-
+        
+        # Iterate through the array, leaving 2 elements for the two pointers.
+        # E.g., for length = 5 (indexes: 0,1,2,3,4), only iterate through index 2 (0,1,2).
         for i in range(len(nums)-2):
+            # As the array is sorted, no three numbers can sum to zero beyond this point.
             if nums[i] > 0: 
                 return res
-
+            
+            # Skip the same element to avoid duplicate triplets.
             if i > 0 and nums[i] == nums[i-1]:
                 continue
 
@@ -15,6 +19,8 @@ class Solution:
 
             while left < right:
                 current = nums[i] + nums[left] + nums[right] 
+                
+                # Check the sum and adjust the pointers accordingly.
                 if current > 0:
                     right -= 1
                 elif current < 0:
@@ -22,7 +28,8 @@ class Solution:
 
                 else: 
                     res.append([nums[i],nums[left],nums[right]])
-
+                    
+                    # Move both pointers for the next potential search.
                     left, right = left + 1, right - 1
                     while left < right and nums[left] == nums[left - 1]:
                         left += 1
