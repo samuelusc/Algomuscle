dd985d194509d60a52deecd13e4549c46874342b
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-01 22:45:05 -0800

    Update and rename 0015-3sum.py to twoPointers-0015-3sum.py
    implement two pointer approach with time & space complexity analytics

--- 0015-3sum/twoPointers-0015-3sum.py
+++ 0015-3sum/twoPointers-0015-3sum.py
@@ -37,3 +37,8 @@ class Solution:
                     while left < right and nums[right] == nums[right + 1]:
                         right -= 1
         return res
+
+        # Time Complexity: sort() -> O(nlogn) + nested loop -> n^2 , then it's O(n^2) 
+        # Space Complexity : in place sort -> O(1) + output-> n^2 then it's O(n^2)
+# If we consider the output space, the worst-case space complexity can be O(n^2) in the scenario where almost all elements are distinct, 
+# leading to a quadratic number of valid triplets.
