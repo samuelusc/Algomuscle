8ede029f1a37c49cddccd91bbceda110ccda88bb
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-04 12:51:53 -0800

    Update kmp-0459-repeated-substring-pattern.py

--- 0459-repeated-substring-pattern/kmp-0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/kmp-0459-repeated-substring-pattern.py
@@ -1,32 +1,38 @@
 class Solution:
-    def nextTable(self, next:List[int], targetStr: str)-> None:
-            j = 0
-            next[0] = 0
+    def nextTable(self, next:List[int], targetStr: str) -> None:
+        # Initialize the pointer for the longest prefix that is also a suffix.
+        j = 0
+        # The first element in the 'next' array is always 0 as a single character
+        # doesn't have any proper prefix or suffix.
+        next[0] = 0
 
-            for i in range(1, len(targetStr)):
-                while j>0 and targetStr[i] != targetStr[j]:
-                    j= next[j-1]
-                
-                if targetStr[i] == targetStr[j]:
-                    j += 1
-                
-                next[i] = j
+        # Iterate through the string to fill up the 'next' array.
+        for i in range(1, len(targetStr)):
+            # If the characters don't match, move the 'j' pointer back accordingly.
+            while j > 0 and targetStr[i] != targetStr[j]:
+                j = next[j-1]
+
+            # If the characters match, increment 'j' as we've found a longer prefix which is also a suffix.
+            if targetStr[i] == targetStr[j]:
+                j += 1
+
+            # Update the 'next' array for the current position.
+            next[i] = j
 
     def repeatedSubstringPattern(self, s: str) -> bool:
-        # constraints s.len <=1
+        # Edge case: if the string length is less than or equal to 1, it can't have a repeating pattern.
+        if len(s) <= 1:
+            return False
 
+        # Initialize the 'next' array for the KMP algorithm.
         next = [0] * len(s)
+        # Fill the 'next' array for the string 's'.
         self.nextTable(next, s)
 
-        # first statement 保证有重复
-        # second 可以被最长前后缀之外的部分整除
-        if next[-1] !=0 and len(s) % (len(s) - next[-1]) == 0 :
-            return True
-        
+        # Check if the last element of the 'next' array is not 0 (indicating a repeated segment) and
+        # the length of the string is a multiple of the length of the repeated substring.
+        if next[-1] != 0 and len(s) % (len(s) - next[-1]) == 0:
+            return True  # The string 's' has a repeated substring pattern.
+
+        # If the conditions are not met, then the string doesn't consist of a repeated substring.
         return False
-   
-        
-
-        
-
-        
