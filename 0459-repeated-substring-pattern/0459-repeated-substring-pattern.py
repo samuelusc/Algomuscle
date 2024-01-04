9c24320221cf9c22cb3ed3c922246b81ebfb5cfd
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-04 12:31:59 -0800

    Time: 85 ms (36.67%), Space: 17.8 MB (5.65%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -1,8 +1,32 @@
 class Solution:
-    def repeatedSubstringPattern(self, s: str) -> bool:
-         # create 2s
-         double_s = s + s
-         index_s = double_s.index(s, 1)
+    def nextTable(self, next:List[int], targetStr: str)-> None:
+            j = 0
+            next[0] = 0
 
-         return index_s < len(s)
-         
+            for i in range(1, len(targetStr)):
+                while j>0 and targetStr[i] != targetStr[j]:
+                    j= next[j-1]
+                
+                if targetStr[i] == targetStr[j]:
+                    j += 1
+                
+                next[i] = j
+
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        # constraints s.len <=1
+
+        next = [0] * len(s)
+        self.nextTable(next, s)
+
+        # first statement 保证有重复
+        # second 可以被最长前后缀之外的部分整除
+        if next[-1] !=0 and len(s) % (len(s) - next[-1]) == 0 :
+            return True
+        
+        return False
+   
+        
+
+        
+
+        
