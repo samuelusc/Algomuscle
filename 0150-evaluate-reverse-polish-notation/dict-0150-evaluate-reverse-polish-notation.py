d6b0c7cbce07cc2f8566a6d0055b74648e75f147
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-06 01:37:04 -0800

    Update and rename 0150-evaluate-reverse-polish-notation.py to dict-0150-evaluate-reverse-polish-notation.py

--- 0150-evaluate-reverse-polish-notation/dict-0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/dict-0150-evaluate-reverse-polish-notation.py
@@ -6,6 +6,8 @@ class Solution:
                 return s[1:].isdigit()
             return s.isdigit()
         stack = []
+        # / 1. operator.truedive(/) 2. floordive (//)
+        # 3. lambda x,y: int(operator.truediv(x,y)) truncates toward zero
         operators = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': lambda x,y: int(operator.truediv(x, y))}
         for token in tokens:
             if  is_digit(token):
