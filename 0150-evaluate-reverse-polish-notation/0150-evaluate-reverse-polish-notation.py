2ec5dce618518ec4dcad30cba8f60963c880f1be
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-06 00:59:54 -0800

    Time: 70 ms (69.25%), Space: 17.9 MB (14.05%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        import operator
+        def is_digit(s):
+            if s[0] == '-':
+                return s[1:].isdigit()
+            return s.isdigit()
+        stack = []
+        operators = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': lambda x,y: int(operator.truediv(x, y))}
+        for token in tokens:
+            if  is_digit(token):
+                stack.append(int(token))
+            
+            elif token in operators:
+                num1 = stack.pop()
+                num2 = stack.pop()
+                # operators['+'](10, 5)  # 等于 10 + 5
+                stack.append(operators[token](num2,num1))
+            else:
+                raise ValueError('Invalid token')
+
+        return stack[-1]
