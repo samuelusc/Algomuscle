6d1e80ca2a697f71d8b3235907074b77030568bb
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-09 18:50:33 -0800

    Time: 49 ms (76.91%), Space: 19 MB (26.19%) - LeetHub

---
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+from collections import deque
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+        
+        res = []
+        queue = deque([root])
+
+        while queue:
+            level = []
+            level_size = len(queue)
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+                # children is a list
+                for child in node.children:
+                    if child:
+                        queue.append(child)
+            
+            res.append(level)
+        
+        return res
