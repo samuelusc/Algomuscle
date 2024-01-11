2e74575ac5fd6c4a5ba13f4176884ab6653ea77e
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-11 01:52:26 -0800

    Time: 168 ms (77.71%), Space: 19.7 MB (12.79%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,17 +1,23 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        left,right,end = 0,len(nums)-1,len(nums)-1
-        res = [0] * len(nums) 
+        #two pointers
 
+        left, right, end = 0, len(nums) - 1, len(nums) -1
+        res = [0] * len(nums)
+
+        # 观察知道最大数要么是最左边的平方，要么最右边的平方
+        # 对新数组赋值从后往前
         while left <= right:
             if nums[left]**2 >= nums[right]**2:
-                res[end] = nums[left]**2
+                res[end] = nums[left] ** 2 
+                
+                end -= 1
                 left += 1
-                end -= 1
+            
             else:
-                res[end] = nums[right]**2
-                right -= 1
+                res[end] = nums[right] ** 2
+
                 end -= 1
+                right -= 1
         
         return res
-
