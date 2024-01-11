2b91a703d439dae936a2a2c4e2f623e20b4978e0
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-10 17:10:53 -0800

    Time: 35 ms (81.58%), Space: 17.2 MB (31.66%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -1,24 +1,25 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         if n == 1:
-            return [[1]]
+            return [[n]]
         
-        row, col, index = 0,0,0
-        directions = ((0,1),(1,0),(0,-1),(-1,0))
-        matrix = [[0]* n for _ in range (n)]
-        
-        for value in range(1,n*n+1):
-            
-            matrix[row][col] = value
-            
-            x, y = directions[index]
-            row_next, col_next = row + x, col + y
+        row, col, index = 0, 0, 0
+        matrix = [[0]*n for _ in range(n)]
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
 
-            if not 0 <= row_next < n or not 0 <= col_next < n or matrix[row_next][col_next] != 0:
-                index = (index +1) % 4
-
+        for i in range(1,n*n+1):
+            matrix[row][col] = i
+                      
             x,y = directions[index]
+            next_row,next_col = row + x, col + y
 
+
+            if not 0<= next_row < n or not 0<= next_col <n or matrix[next_row][next_col] != 0:
+                index = (index + 1) % 4
+
+            x,y = directions[index]           
             row, col = row + x, col + y
         
         return matrix
+             
+        
