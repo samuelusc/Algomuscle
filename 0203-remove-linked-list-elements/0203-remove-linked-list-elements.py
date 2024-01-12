1286117fe37dbe3238fbcf0665c4bb099871a211
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-12 00:49:51 -0800

    Time: 34 ms (99.8%), Space: 20.1 MB (50.61%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -5,17 +5,19 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        
-        dummy = ListNode(0)
-        dummy.next = head
-
+        # 建立dummy 方便检查头节点
+        dummy = ListNode(next = head)
         cur_node = dummy
 
+        #需要有pre_node
         while cur_node.next:
             if cur_node.next.val == val:
                 cur_node.next = cur_node.next.next
-            
+
+            # 保证next节点不是target
             else:
                 cur_node = cur_node.next
         
         return dummy.next
+
+                      
