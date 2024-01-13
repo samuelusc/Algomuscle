e12ae324e62a1fecc64f3b12f92215badd65f8d2
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-12 19:37:18 -0800

    Time: 27 ms (98.76%), Space: 17.7 MB (7.39%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -6,18 +6,32 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        res = self.traverse(root)
+        if res != 0:
+            return res
+        else:
+            return 0
+
+    # 递归函数的参数和返回值
+    def traverse(self, root):    
+        #base case 递归终止条件 1
         if not root:
             return 0
-
+        #递归终止条件 2 ->叶子节点
         if not root.left and not root.right:
             return 0
+        
+        #单层递归逻辑
+        #先递归 -> 后检查是否叶节点
+        leftNums = self.traverse(root.left)
 
-        leftValue = self.sumOfLeftLeaves(root.left)
+        #回溯时再处理节点
         if root.left and not root.left.left and not root.left.right:
-            leftValue = root.left.val
+            leftNums = root.left.val
+        
+        #单层递归逻辑
+        rightNums = self.traverse(root.right)
 
-        rightValue = self.sumOfLeftLeaves(root.right)
-
-        sum_val = leftValue + rightValue
-
-        return sum_val 
+        res = leftNums + rightNums
+        #递归返回值 ->int
+        return res
