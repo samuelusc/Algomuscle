7f9e2a71bcba09abdb039ccfa97c77ebc4ee58eb
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-12 22:07:53 -0800

    Time: 39 ms (63.82%), Space: 17.4 MB (18.9%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -6,30 +6,26 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        # 前序遍历
+        res = []
         path = []
-        result = []
         if not root:
-            return result
-        self.dfs(root, path, result)
-        return result
-    
+            return res
+        self.preOrder(root, path, res)
+        return res
 
-    def dfs(self, cur_node, path, result):
-        # 父节点 （中）
-        path.append(cur_node.val)
-        # 到达叶子节点
-        if not cur_node.left and not cur_node.right:
-            result.append('->'.join(map(str,path)))
-            return 
+    def preOrder(self, node, path, res):
+        
 
-        if cur_node.left:
-            self.dfs(cur_node.left, path, result)
+        path.append(str(node.val))
+
+        if not node.left and not node.right:
+            res.append('->'.join(path))
+            return
+        
+        if node.left:
+            self.preOrder(node.left, path, res)
             path.pop()
         
-        if cur_node.right:
-            self.dfs(cur_node.right, path, result)
+        if node.right:
+            self.preOrder(node.right, path, res)
             path.pop()
-
-
-    
