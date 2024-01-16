0a1781aa1483baf3b9c4960e8da5d6e2b3bdbf94
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-15 23:07:43 -0800

    Update conciseCounter-0454-4sum-ii.py

--- 0454-4sum-ii/conciseCounter-0454-4sum-ii.py
+++ 0454-4sum-ii/conciseCounter-0454-4sum-ii.py
@@ -1,5 +1,9 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        # 使用Counter计算 {Key: a+b Value: count}
         from collections import Counter
+        
+        # list comprehension 计算a+b 的数量并生成一个dictionary
         ab_sum = Counter(a+b for a in nums1 for b in nums2)
+        
         return sum(ab_sum[-(c+d)]for c in nums3 for d in nums4)
