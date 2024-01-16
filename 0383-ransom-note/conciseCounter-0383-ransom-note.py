f56a5774c33ca83bc4ff6e98e76be57d79a9015f
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-15 23:52:24 -0800

    Update conciseCounter-0383-ransom-note.py

--- 0383-ransom-note/conciseCounter-0383-ransom-note.py
+++ 0383-ransom-note/conciseCounter-0383-ransom-note.py
@@ -4,4 +4,8 @@ class Solution:
         # Subtracting Counters will only retain positive counts
         # and remove negative and zero counts
         return not Counter(ransomNote) - Counter(magazine)
+        # 如果magzine 中每个字符数量大于等于ransomNote,则返回空        
+
+
         # return Counter(ransomNote) <= Counter(magazine)
+        # 检查第一个计数器中的每个计数是否都不大于第二个计数器中相应字符的计数
