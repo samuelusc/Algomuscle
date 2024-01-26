c5fe8db3926002411d65c46fcd83c4334c45a923
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-25 20:20:12 -0800

    Update backtracking-0491-non-decreasing-subsequences.py

--- 0491-non-decreasing-subsequences/backtracking-0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/backtracking-0491-non-decreasing-subsequences.py
@@ -6,6 +6,7 @@ class Solution:
         def backtracking(nums, startIndex):
             # 这里是开始记录的条件，而不是递归结束条件
             if len(path) > 1:
+                #结果在树枝和树叶
                 res.append(path[:])
             # 为什么没有加return 因为会跳过后面的元素，而这里并非截止点
 
