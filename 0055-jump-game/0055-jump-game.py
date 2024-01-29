8a56d74f9fe769e8e334fd2da12871a275f5735f
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-28 20:00:15 -0800

    Time: 363 ms (77.74%), Space: 17.8 MB (65.05%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -8,7 +8,8 @@ class Solution:
         if len(nums) == 1:
             return True
 
-        # cover 代表 index,下面没有包括[1]一个元素情况
+        # cover 代表 index,下面没有包括nums 只一个元素情况
+        # should consider if i is over the boundary of len(nums)
         while i < len(nums) and i <= cover:
             cover = max(nums[i]+ i, cover)
             if cover >= len(nums) - 1:
