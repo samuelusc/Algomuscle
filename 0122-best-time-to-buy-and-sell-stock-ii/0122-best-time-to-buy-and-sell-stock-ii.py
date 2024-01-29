334b8883aa1bb57ed562f172112d70137959075d
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-29 15:05:14 -0800

    Time: 55 ms (85%), Space: 17.6 MB (90.56%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,13 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res = 0
+        
+        count = 0
+        for i in range(len(prices) -1):
+            count = count + max(prices[i+1] - prices[i], 0)
 
-        for i in range(1,len(prices)):
-            res = res + max(prices[i] - prices[i-1], 0)
-
-        return res
-
-
-        #p(1）买 p(3) 卖 相当于 p(3)-p(2) + p(2)-p(1) 
-        # once the array is in monotonically decreasing order, 0 is output
-        # we can pick the max(0, with p[i]-p[i-1])
+        return count 
