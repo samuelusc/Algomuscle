b3a8f60260d003f98820a96e1f9ddf0faf073bd8
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-01-30 00:18:42 -0800

    Time: 44 ms (86.59%), Space: 16.8 MB (55.19%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashmap = {}
+        res = set()
+        for num in nums1:
+            hashmap[num] = hashmap.get(num, 0) + 1
+
+        for num in nums2:
+            if num in hashmap:
+                res.add(num)
+
+
+        return list(res)
