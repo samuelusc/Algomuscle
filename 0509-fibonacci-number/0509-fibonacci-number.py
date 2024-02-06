7497d19010a4a520f65333b9d3b584cce50a994f
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-02-05 17:56:30 -0800

    Time: 36 ms (72.3%), Space: 16.5 MB (61.9%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fib(self, n: int) -> int:
+    
+        if n <=1:
+            return n
+        
+        dp = [0,1]
+
+        for i in range(2, n+1):
+            sum = dp[0] + dp[1]
+            dp[0] = dp [1]
+            dp[1] = sum
+
+        return sum
