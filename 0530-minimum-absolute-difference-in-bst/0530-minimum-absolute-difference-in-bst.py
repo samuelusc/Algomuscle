c7fef7aafc27f89d45b6f97ae92dfea80ff459b0
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-02-16 23:36:49 -0800

    Time: 44 ms (81.3%), Space: 18.1 MB (86.67%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
@@ -5,33 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.dif = float('inf')
+        self.pre_node = None
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        # inorder traversal
-        # BST 单调递增->中序遍历
+        self.inorder(root)
+        return self.dif
 
-        res = float('inf')
-        pre_node = None
+    def inorder(self, cur_node):
+        if not cur_node:
+            return
 
-        def inorder(cur_node):
-            # base case 
-            if not cur_node:
-                return 
-            # 左子树递归
-            inorder(cur_node.left)
+        self.inorder(cur_node.left)
 
-            #define immutable variable global
-            nonlocal res, pre_node
+        if self.pre_node:
+            self.dif = min(self.dif, cur_node.val - self.pre_node.val)
+        
+        self.pre_node = cur_node
 
-            # 处理中间节点
-            if pre_node:
-                res = min(res, cur_node.val - pre_node.val)
-            # previous node 指向current node
-            pre_node = cur_node
-            
-            # 右子树递归 
-            inorder(cur_node.right)
-
-        inorder(root)
-        return res
-
-
+        self.inorder(cur_node.right)
