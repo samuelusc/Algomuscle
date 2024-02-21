861575eb70898c8df2785b55a828d1469d014200
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-02-20 19:11:33 -0800

    Time: 85 ms (91.7%), Space: 16.7 MB (95.57%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,22 +1,23 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        total_sum = sum(nums)
-
-        if total_sum < abs(target) or (total_sum + target) % 2 != 0:
+        
+        total = sum(nums)
+        if total < abs(target) or ((total + target) % 2 != 0):
             return 0
 
-        sub = (total_sum + target) // 2
+        bag_size = (total + target)//2
+        dp = [[0]*(bag_size+1) for _ in range(len(nums))]
 
-        dp = [[0] * (sub + 1) for _ in range (len(nums) + 1)]
+        dp[0][0] = 1 if nums[0] != 0 else 2
+        for j in range(1, bag_size+1):
+            if nums[0] == j:
+                dp[0][j] = 1
 
+        for i in range(1, len(nums)):
+            for j in range(0, bag_size+1):
+                if j >= nums[i]:
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i]]
+                else:
+                    dp[i][j] = dp[i-1][j]
 
-        dp[0][0] = 1
-
-        for i in range (1, len(nums) + 1):
-            for j in range(sub + 1):
-                dp[i][j] = dp[i-1][j]
-                if j >= nums[i-1]:
-                    dp[i][j] += dp[i-1][j-nums[i-1]]
-
-        
-        return dp[-1][sub]
+        return dp[-1][-1]
