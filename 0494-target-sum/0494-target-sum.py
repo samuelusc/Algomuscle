b74b280d035560d8e398f06099c11ffb93f01e79
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-02-20 23:53:23 -0800

    Time: 58 ms (97.78%), Space: 16.6 MB (95.57%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,23 +1,21 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        
         total = sum(nums)
-        if total < abs(target) or ((total + target) % 2 != 0):
+
+        size = (total + target) // 2
+
+        if abs(total) < abs(target) or (total + target) % 2 != 0:
             return 0
 
-        bag_size = (total + target)//2
-        dp = [[0]*(bag_size+1) for _ in range(len(nums))]
+        dp = [0] * (size + 1)
+        
+        # Base case: one way to sum to 0 (using no numbers)
+        dp[0] = 1 
 
-        dp[0][0] = 2 if nums[0] == 0 else 1
-        for j in range(1, bag_size+1):
-            if nums[0] == j:
-                dp[0][j] = 1
+        for num in nums:
+            for j in range(size, num - 1, -1):
+                if j >= num:
+                    dp[j] += dp[j-num]
+        
 
-        for j in range(bag_size + 1):
-            for i in range(1, len(nums)):
-                if j >= nums[i]:
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i]]
-                else:
-                    dp[i][j] = dp[i-1][j]
-
-        return dp[-1][-1]
+        return dp[-1]
