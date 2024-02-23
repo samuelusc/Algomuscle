8f804b07b2258563d8b7f27abc0538d87315cbd9
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-02-23 01:19:05 -0800

    Time: 46 ms (82.63%), Space: 16.6 MB (92.34%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
@@ -2,16 +2,12 @@ class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
         
         target = sum(stones) // 2
-
-
         dp = [0] * (target + 1)
 
-        for num in stones:
-            for j in range(target, num-1, -1):
-                dp[j] = max(dp[j],dp[j-num]+ num)
+        for stone in stones:
+            for j in range(target, stone - 1, -1):
+                dp[j] = max(dp[j], dp[j-stone] + stone)
 
-        
-        
-        res = sum(stones) - 2 * dp[-1] 
+        res = sum(stones) - 2 * dp[target]
 
         return res
