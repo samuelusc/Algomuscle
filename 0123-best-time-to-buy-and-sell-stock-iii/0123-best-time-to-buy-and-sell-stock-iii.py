d2aa1705edfe3a3374d6e707f2f2e5ac513a9156
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-03-04 19:09:03 -0800

    Time: 985 ms (32.44%), Space: 37.9 MB (28.13%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,16 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = [[0] * 5 for _ in range(len(prices))]
-        dp[0][0] = 0
-        dp[0][1] = -prices[0]
-        dp[0][2] = 0
-        dp[0][3] = -prices[0]
-        dp[0][4] = 0
+        # deploy DP to solve it 
+        dp = [[0] * 4 for _ in range(len(prices))] 
+        dp[0][0] = -prices[0] # first transaction
+        dp[0][1] = 0
+        dp[0][2] = -prices[0] # second transaction
+        dp[0][3] = 0
         for i in range(1,len(prices)):
-            dp[i][0] = dp[i-1][0]
-            dp[i][1] = max(dp[i-1][1], -prices[i])
-            dp[i][2] = max(dp[i-1][2], dp[i-1][1] + prices[i])
-            dp[i][3] = max(dp[i-1][3], dp[i-1][2] - prices[i])
-            dp[i][4] = max(dp[i-1][4],dp[i-1][3] + prices[i])
+            # define four states 
+            dp[i][0] = max(dp[i-1][0], -prices[i])
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])
+            dp[i][2] = max(dp[i-1][2], dp[i-1][1] - prices[i])
+            dp[i][3] = max(dp[i-1][3],dp[i-1][2] + prices[i])
 
-        return dp[-1][4]
+        return dp[-1][3]
