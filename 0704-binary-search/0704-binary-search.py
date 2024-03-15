ea2c9532bd3c03aeb0fb6387121093b5933af057
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-03-15 01:08:25 -0700

    Time: 194 ms (45.96%), Space: 18.3 MB (23.57%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # left is included and right is include
+
+        left, right = 0, len(nums) - 1
+        
+        # 保持循环不变量
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] < target:
+                left = mid + 1
+            
+            elif nums[mid] > target:
+                # 闭空间 -1
+                right = mid -1
+
+            else:
+                return mid
+
+        return -1  
+                
