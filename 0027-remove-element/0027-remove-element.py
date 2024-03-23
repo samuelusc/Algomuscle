38e44863c278689be8ce9185e53d8151f2e533f9
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-03-22 20:04:02 -0700

    Time: 44 ms (12.39%), Space: 16.5 MB (49.67%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        slow = 0
+
+        for fast in nums:
+            if fast != val:
+                nums[slow] = fast
+            
+                slow += 1
+        
+        return slow
