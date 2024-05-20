9a516f04cb1cc9f2e9de364ca5ae74784df22260
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-05-19 23:01:05 -0700

    Time: 29 ms (92.16%), Space: 16.7 MB (49.12%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -6,23 +6,30 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        # Call the helper function postOrder to compute the sum of left leaves
         res = self.postOrder(root)
+        # Return the result
         return res
     
-
     def postOrder(self, node):
+        # If the current node is None, return 0
         if not node:
             return 0
 
+        # If the current node is a leaf node, return 0
         if not node.left and not node.right:
             return 0
 
+        # Recursively calculate the sum of left leaves in the left subtree
         leftNums = self.postOrder(node.left)
         
+        # If the left child exists and is a leaf node, add its value to leftNums
         if node.left and not node.left.left and not node.left.right:
             leftNums = node.left.val
         
+        # Recursively calculate the sum of left leaves in the right subtree
         rightNums = self.postOrder(node.right)
     
+        # Return the sum of leftNums and rightNums
         return leftNums + rightNums
     
