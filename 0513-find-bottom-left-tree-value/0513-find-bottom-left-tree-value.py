bafba1889d6ec944b9d7961a7d3cd47c9506a489
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-05-20 21:39:47 -0700

    Time: 52 ms (18.29%), Space: 18.2 MB (80.84%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -5,24 +5,36 @@
 #         self.left = left
 #         self.right = right
 from collections import deque
+
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        # Initialize a deque for level order traversal, starting with the root node
         queue = deque([root])
 
+        # Variable to store the left-most value
         left_most = None
 
+        # Perform level order traversal using the queue
         while queue:
+            # Get the number of nodes at the current level
             size = len(queue)
 
+            # Traverse all nodes at the current level
             for i in range(size):
+                # Pop the node from the front of the queue
                 node = queue.popleft()
+
+                # Update left_most with the value of the first node at the current level
                 if i == 0:
                     left_most = node.val
 
+                # Add the left child to the queue if it exists
                 if node.left:
                     queue.append(node.left)
                 
+                # Add the right child to the queue if it exists
                 if node.right:
                     queue.append(node.right)
 
+        # Return the left-most value at the bottom level
         return left_most
