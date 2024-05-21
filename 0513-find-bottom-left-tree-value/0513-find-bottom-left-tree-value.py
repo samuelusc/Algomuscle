ad8331e4294bb5b043c919011cd8b1833c9271d5
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-05-20 22:02:10 -0700

    Time: 44 ms (66.74%), Space: 19.6 MB (21.17%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -4,37 +4,30 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
-
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        # Initialize a deque for level order traversal, starting with the root node
-        queue = deque([root])
+        
+        self.maxDepth = float('-inf')
+        self.res = None
 
-        # Variable to store the left-most value
-        left_most = None
+        self.traversal(root, 1)
+        return self.res
+    
+    def traversal(self, node, depth):
+        
+        if not node.left and not node.right:
+            if self.maxDepth < depth:
+                self.maxDepth = depth
+                self.res =  node.val
+            return 
 
-        # Perform level order traversal using the queue
-        while queue:
-            # Get the number of nodes at the current level
-            size = len(queue)
-
-            # Traverse all nodes at the current level
-            for i in range(size):
-                # Pop the node from the front of the queue
-                node = queue.popleft()
-
-                # Update left_most with the value of the first node at the current level
-                if i == 0:
-                    left_most = node.val
-
-                # Add the left child to the queue if it exists
-                if node.left:
-                    queue.append(node.left)
-                
-                # Add the right child to the queue if it exists
-                if node.right:
-                    queue.append(node.right)
-
-        # Return the left-most value at the bottom level
-        return left_most
+        if node.left:
+            depth += 1
+            self.traversal(node.left, depth)
+            depth -= 1
+        if node.right:
+            depth += 1
+            self.traversal(node.right, depth)
+            depth -= 1
+        
+        
