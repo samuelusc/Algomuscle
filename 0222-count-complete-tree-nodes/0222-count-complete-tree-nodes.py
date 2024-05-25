ff9a80aea333612256b9ace8fd30f8abc51412c3
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-05-25 15:00:45 -0700

    Time: 45 ms (96.14%), Space: 21.8 MB (63.88%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -6,28 +6,17 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        # If the tree is empty, return 0
         if not root:
             return 0
+        
+        left, right = root.left, root.right
+        level = 1
 
-        countNum = 1  # Initialize the counter to 1 to include the root node
-
-        left, right = (
-            root.left,
-            root.right,
-        )  # Initialize pointers for left and right subtrees
-
-        # Traverse the left and right subtree heights
         while left and right:
-            countNum += 1  # Increment the counter for each level
-            left, right = (
-                left.left,
-                right.right,
-            )  # Move to the next level of left and right subtrees
-
-        # If left and right both reach leaf nodes, it's a full binary tree
+            left,right = left.left, right.right
+            level += 1
+        
         if not left and not right:
-            return 2**countNum - 1  # Full binary tree node count formula: 2^h - 1
-
-        # If it's not a full binary tree, recursively count nodes in left and right subtrees and add 1 for the root
+            return 2 ** level - 1
+        
         return 1 + self.countNodes(root.left) + self.countNodes(root.right)
