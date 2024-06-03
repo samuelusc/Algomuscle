9f355017bdc6caea30fe4f9d5a828238c6fd768e
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-06-02 22:34:31 -0700

    Time: 37 ms (93.11%), Space: 17.5 MB (82.66%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,31 +6,25 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        #Define a helper function to calculate the height of the tree
-        def _getHeight(node):
-            # If the node is None, the height is 0
+        
+        def _getHeight(node: Optional[TreeNode]) ->int:
             if not node:
                 return 0
-            # Recursively calculate the height of the left subtree
-            left_tree = _getHeight(node.left)
-            if left_tree == -1:
-                return -1
 
-            # Recursively calculate the height of the right subtree
-            right_tree = _getHeight(node.right)
-            # If the right subtree is unbalanced, return -1
-            if right_tree == -1:
+            leftH = _getHeight(node.left)
+            if leftH == -1:
                 return -1
+            
 
-            # if the diff between left and right sbutrees is greater than 1
-            # return -1 indicating unbalanced
-            if abs(left_tree - right_tree) > 1:
+            rightH = _getHeight(node.right)
+            if rightH == -1:
                 return -1
-            # return the height of the current node
-            return 1 + max(left_tree, right_tree)
+            
 
-        # call the helper function to check the root node
-        res = _getHeight(root)
-        # If the result is not -1, the tree is balanced, return True
-        # Otherwise return False
-        return res != -1
+            if abs(leftH - rightH) > 1:
+                return -1
+            
+            return 1 + max(leftH, rightH)
+        
+        height = _getHeight(root)
+        return height != -1 
