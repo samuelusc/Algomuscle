f10eb4a6a9e08ecabeebdea753588b1b5824e019
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-06-30 22:36:29 -0700

    Time: 46 ms (72.55%), Space: 17 MB (48.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,7 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        record = [0] * 26
+        a = Counter(s)
+        b = Counter(t)
 
-        for char in s:
-            record[ord(char) - ord('a')] += 1
-        
-        for char in t:
-            record[ord(char) - ord('a')] -= 1
-
-        for num in record:
-            if num:
-                return False
-
-        return True
+        return a == b
