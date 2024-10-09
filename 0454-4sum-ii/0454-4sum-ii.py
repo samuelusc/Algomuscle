d1037bb11437159fc9d44afb03ff88a0d863c851
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-10-09 15:01:08 -0400

    Time: 402 ms (75.86%), Space: 16.9 MB (27.87%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        dict1 = {}
-        count = 0
+        
+        hashmap = {}
         for i in nums1:
             for j in nums2:
-                dict1[i+j]= dict1.get(i+j, 0) + 1
-        
+                hashmap[i+j]= hashmap.get(i+j, 0) + 1
+
+        count = 0
         for x in nums3:
             for y in nums4:
-                target = -(x+y)
-                count += dict1.get(target, 0)
+                count += hashmap.get(-(x+y), 0)
         
         return count
