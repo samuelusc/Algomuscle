bc7c5b05333621e28c2e699a5bd79ceba1f333d1
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-10-22 22:43:07 -0400

    Time: 33 ms (67.21%), Space: 16.5 MB (78.71%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -16,8 +16,8 @@ class Solution:
             node = stack.pop()
             if node:
                 res.append(node.val)
-                stack.append(node.left)
                 stack.append(node.right)
+                stack.append(node.left)
         
         return res
         
