e15963bf7e0bf8594fd3a3f6937bd92473917bf6
samuelusc <97491762+samuelusc@users.noreply.github.com>
2024-10-28 23:42:24 -0400

    Time: 0 ms (100%), Space: 16.6 MB (73.95%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,15 +6,21 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def isMirror(node1,node2):
-            if not node1 and not node2:
-                return True
+        from collections import deque
+        dq = deque([root.left, root.right])
 
+        while dq:
+            node1 = dq.popleft()
+            node2 = dq.popleft()
+
+            if not node1 and not node2:
+                continue
             if not node1 or not node2 or node1.val != node2.val:
                 return False
             
-            outside = isMirror(node1.left,node2.right)
-            inside = isMirror(node1.right, node2.left)
-            return outside and inside
+            dq.append(node1.left)
+            dq.append(node2.right)
+            dq.append(node1.right)
+            dq.append(node2.left)
         
-        return isMirror(root.left, root.right)
+        return True
